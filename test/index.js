f191dbe25c712706693aff55dc9812105603cb2d
Ionică Bizău <bizauionica@yahoo.com>
2013-10-05 21:43:44 +0300

    Added tests.

---
+++ test/index.js
@@ -0,0 +1,35 @@
+// require Johnny's static
+var JohhnysStatic = require("../index.js"),
+    http = require('http');
+
+// set static server: public folder
+JohhnysStatic.setStaticServer({root: "./public"});
+
+// set routes
+JohhnysStatic.setRoutes({
+    "/":       { "url": "/html/index.html" },
+    "/test1/": { "url": "/html/test1.html" },
+    "/test2/": { "url": "/html/test2.html" }
+});
+
+// create http server
+http.createServer(function(req, res) {
+    // safe serve
+    if (JohhnysStatic.exists(req, res)) {
+        // serve file
+        JohhnysStatic.serve(req, res, function (err) {
+            // not found error
+            if (err.code === "ENOENT") {
+                res.end("404 - Not found.");
+                return;
+            }
+
+            // other error
+            res.end(JSON.stringify(err));
+        });
+        return;
+    }
+
+    // if the route doesn't exist, it's a 404!
+    res.end("404 - Not found");
+}).listen(8000);
