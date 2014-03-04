08bc3b1c5d264d9081dfc4fdcea390bb77164563
Ionică Bizău <bizauionica@yahoo.com>
2014-03-04 20:50:58 +0200

    Set 404 status code.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 // require Johnny's static
-var JohhnysStatic = require("johnnys-node-static"),
+var JohhnysStatic = require("../index"),
     http = require('http');
 
 // set static server: public folder
@@ -7,9 +7,9 @@ JohhnysStatic.setStaticServer({root: "./public"});
 
 // set routes
 JohhnysStatic.setRoutes({
-    "/":       { "url": "/html/index.html" },
-    "/test1/": { "url": "/html/test1.html" },
-    "/test2/": { "url": "/html/test2.html" }
+    "/":       { "url": __dirname + "/html/indexaaaa.html" },
+    "/test1/": { "url": __dirname + "/html/test1.html" },
+    "/test2/": { "url": __dirname + "/html/test2.html" }
 });
 
 // create http server
@@ -18,8 +18,10 @@ http.createServer(function(req, res) {
     if (JohhnysStatic.exists(req, res)) {
         // serve file
         JohhnysStatic.serve(req, res, function (err) {
+
             // not found error
             if (err.code === "ENOENT") {
+                res.writeHead(404, {"Content-Type": "text/html"});
                 res.end("404 - Not found.");
                 return;
             }
