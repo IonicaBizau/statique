bd647214ec8623acd70db1c78a4a09b09c75a985
Ionică Bizău <bizauionica@yahoo.com>
2014-04-27 10:46:10 +0300

    Simplified the code

--- test/index.js
+++ test/index.js
@@ -1,26 +1,21 @@
-// require statique
+// dependencies
 var Statique = require("../index")
-
-    // require http
   , http = require('http')
   ;
 
-// set static server: public folder
-Statique.server({root: __dirname + "/public"});
+// statique config
+Statique
+    .server({root: __dirname + "/public"})
+    .setRoutes({
+        "/":       "/html/index.html"
+      , "/test1/": "/html/test1.html"
+      , "/test2/": "/html/test2.html"
+    })
+  ;
 
-// set routes
-Statique.setRoutes({
-    "/":       "/html/index.html"
-  , "/test1/": "/html/test1.html"
-  , "/test2/": "/html/test2.html"
-});
-
-// create http server
+// create server
 http.createServer(function(req, res) {
-
-    // serve file
     Statique.sendRes (req, res);
-
 }).listen(8000);
 
 // output
