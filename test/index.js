24c50519ac0bebcab41c4c0252be29e4a51ae59b
Ionică Bizău <bizauionica@yahoo.com>
2014-06-23 21:25:51 +0300

    Removed obsolete condition

--- test/index.js
+++ test/index.js
@@ -34,13 +34,9 @@ Statique
       , 500: "/html/errors/500.html"
     });
   ;
+
 // create server
-http.createServer(function(req, res) {
-    if (req.url === "/500") {
-        return Statique.sendRes(res, 500, "html", "This is supposed to be a 500 Internal server error page");
-    }
-    Statique.serve(req, res);
-}).listen(8000);
+http.createServer(Statique.serve).listen(8000);
 
 // output
 console.log("Listening on 8000.");
