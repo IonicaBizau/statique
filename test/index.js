31868d581435d064d0f4ea8a1f19808bb6efd55b
Ionică Bizău <bizauionica@yahoo.com>
2014-09-28 22:10:53 +0300

    Updated tests

--- test/index.js
+++ test/index.js
@@ -1,55 +1,54 @@
-// dependencies
+// Dependencies
 var Statique = require("../index")
-  , http = require('http')
+  , Http = require('http')
   ;
 
-// statique config
-Statique
-    .server({root: __dirname + "/public"})
-    .setRoutes({
-        "/":       "/html/index.html"
-      , "/test1/": {url: "/html/test1.html"}
-      , "/test2": "/html/test2.html"
-      , "/some/api": function (req, res) {
-            res.end("Hello World!");
+// Create *Le Statique* server
+var server = new Statique({
+    root: __dirname + "/public"
+  , cache: 36000
+}).setRoutes({
+    "/":       "/html/index.html"
+  , "/test1/": {url: "/html/test1.html"}
+  , "/test2": "/html/test2.html"
+  , "/some/api": function (req, res) {
+        res.end("Hello World!");
+    }
+  , "/some/test1-alias": function (req, res) {
+        Statique.serveRoute("/test1", req, res);
+    }
+  , "/method-test": {
+        get: function (req, res) { res.end("GET"); }
+      , post: function (req, res, form) {
+            form.on("done", function (form) {
+                console.log(form.data);
+            });
+            res.end();
         }
-      , "/some/test1-alias": function (req, res) {
-            Statique.serveRoute("/test1", req, res);
+    }
+  , "/crash": {
+        get: function (req, res) { undefined.something; }
+    }
+  , "\/anynumber\-[0-9]": {
+        type: "regexp"
+      , handler: function (req, res) {
+            res.end("Hi");
         }
-      , "/method-test": {
-            get: function (req, res) { res.end("GET"); }
-          , post: function (req, res, form) {
-                form.on("done", function (form) {
-                    console.log(form.data);
-                });
-                res.end();
-            }
+    }
+  , "r1": {
+        type: "regexp"
+      , regexp: /anyletter\-[a-z]/i
+      , handler: function (req, res) {
+            res.end("Case insensitive is important. ;)");
         }
-      , "/crash": {
-            get: function (req, res) { undefined.something; }
-        }
-      , "\/anynumber\-[0-9]": {
-            type: "regexp"
-          , handler: function (req, res) {
-                res.end("Hi");
-            }
-        }
-      , "r1": {
-            type: "regexp"
-          , regexp: /anyletter\-[a-z]/i
-          , handler: function (req, res) {
-                res.end("Case insensitive is important. ;)");
-            }
-        }
-    })
-    .setErrors({
-        404: "/html/errors/404.html"
-      , 500: "/html/errors/500.html"
-    });
-  ;
+    }
+}).setErrors({
+    404: "/html/errors/404.html"
+  , 500: "/html/errors/500.html"
+});
 
 // create server
-http.createServer(Statique.serve).listen(8000);
+Http.createServer(server.serve).listen(8000);
 
-// output
+// Output
 console.log("Listening on 8000.");
